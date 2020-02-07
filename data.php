7fe109ba7b536b9353894142127f2a95d777cc42
Muhamad Yulianto <mail@mjniuz.com>
2020-02-07 14:53:34 +0700

    add page

---
+++ data.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$paginate   = !empty($_GET['page']) ? $_GET['page'] : 0;
+
+echo getData($paginate);
+
+function getData($paginate = 0){
+    $url    = 'http://importir.local/api/get-crowdfund-indonesia?page=' . $paginate;
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_POST, 0);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch);
+
+    curl_close ($ch);
+
+    $result     = json_decode($server_output);
+
+    if(count($result->data) == 0){
+        return json_encode([]);
+    }
+
+    $response   = [];
+    foreach ($result->data as $datum){
+        $text   = (strlen($datum->title_en) > 60) ? substr($datum->title_en, 0, 57) . '...' : $datum->title_en;
+        $response[] = [
+            'product_id'    => $datum->product_id,
+            'title_en'      => $text,
+            'image'         => $datum->image,
+            'price'         => number_format($datum->price_only,0),
+            'wa_link'       => 'https://api.whatsapp.com/send?phone=6281398080314&text=Hallo,%20saya%20ingin%20bertanya%20produk%20SKU%20'. $datum->product_id .',%20apakah%20masih%20ada'
+        ];
+    }
+
+    return json_encode($response);
+
+// Further processing ...
+    if ($server_output == "OK") {
+
+    } else {
+
+    }
+}
--- index.php
+++ index.php
@@ -19,6 +19,16 @@ body {
     text-align: left;
     background-color: #fff;
 }
+    .card-body{
+        height: 215px;
+    }
+    .absolute-bottom{
+        position: absolute;
+        bottom: 10px;
+    }
+    .image-loading{
+        width: 100%;
+    }
 </style>
 </head>
 <body class="bg-light">
@@ -29,107 +39,103 @@ body {
 				<h4 class="text-center">Dapatkan Produk Terbaik dengan Harga GILA-GILAAN, Silahkan Pilih dan kontak CS Kami untuk membeli produk</h4>
 			</div>
 		</div>
-		<div class="row mt-3">
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
-			<div class="col-md-3 mt-3">
-				<div class="card">
-					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
-				  <div class="card-body">
-				    <h5 class="card-title">Jam Tangan Brush V2</h5>
-				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
-				    <div class="text-center">
-				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
-				    </div>
-				  </div>
-				</div>
-			</div>
+		<div class="row mt-3" id="result-here" data-page="0">
 		</div>
+
+        <div class="row mt-3">
+            <div class="text-center image-loading">
+                <img src="./loading.gif" id="loading"/>
+            </div>
+            <div class="text-center image-loading">
+                <button id="load-more" class="btn btn-primary" style="display: none">
+                    Load More...
+                </button>
+            </div>
+        </div>
 	</div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="./endles-scroll.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            const theURL    = '<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ;?>';
+
+            let thePage     = $('#result-here').data('page');
+            $.ajax({
+                url: theURL + 'data.php?page=' + thePage,
+                dataType: 'html',
+                success: function(result) {
+                    let responseResult  = JSON.parse(result);
+                    let HTML    = '';
+                    $.each(responseResult, function (key, val) {
+                        HTML    += '' +
+                            '<div class="col-md-3 mt-3">\n' +
+                            '<div class="card">\n' +
+                            '<img src="' + val.image + '" class="card-img-top" height="250">\n' +
+                            '<div class="card-body">\n' +
+                            '<h5 class="card-title">'+ val.title_en +'</h5>\n' +
+                            '<div class="absolute-bottom">' +
+                            '<p class="card-text">Harga Mulai <b>Rp '+ val.price +'</b></p>\n' +
+                            '<div class="text-center">\n' +
+                            '<a href="'+ val.wa_link +'" target="_blank" class="btn btn-success">WhatsApp Sekarang</a>\n' +
+                            '</div>' +
+                            '</div>\n' +
+                            '</div>\n' +
+                            '</div>\n' +
+                            '</div>';
+                    });
+                    $('#result-here').data('page', (thePage+1));
+                    $('#result-here').append(HTML);
+
+                    $('#loading').hide();
+                    $('#load-more').show();
+                }
+            });
+
+            $('#load-more').on('click', function(e){
+                $(this).text('Loading...');
+                $(this).attr('disabled', true);
+
+                let newThis = $(this);
+
+                $('#loading').show();
+                // ajax
+                let thePage     = $('#result-here').data('page');
+                $.ajax({
+                    url: theURL + 'data.php?page=' + thePage,
+                    dataType: 'html',
+                    success: function(result) {
+                        let responseResult  = JSON.parse(result);
+                        let HTML    = '';
+                        $.each(responseResult, function (key, val) {
+                            HTML    += '' +
+                                '<div class="col-md-3 mt-3">\n' +
+                                '<div class="card">\n' +
+                                '<img src="' + val.image + '" class="card-img-top" height="250">\n' +
+                                '<div class="card-body">\n' +
+                                '<h5 class="card-title">'+ val.title_en +'</h5>\n' +
+                                '<div class="absolute-bottom">' +
+                                '<p class="card-text">Harga Mulai <b>Rp '+ val.price +'</b></p>\n' +
+                                '<div class="text-center">\n' +
+                                '<a href="'+ val.wa_link +'" target="_blank" class="btn btn-success">WhatsApp Sekarang</a>\n' +
+                                '</div>' +
+                                '</div>\n' +
+                                '</div>\n' +
+                                '</div>\n' +
+                                '</div>';
+                        });
+                        $('#result-here').data('page', (thePage+1));
+                        $('#result-here').append(HTML);
+
+                        $('#loading').hide();
+
+                        newThis.text('Load More...');
+                        newThis.attr('disabled', false);
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
