4a64fd036e1fa18180ddeea121fa85e926f532d8
kamsitdigital <kamsitdigital@gmail.com>
2020-02-07 10:50:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cuci Gudang Indonesia</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="/img/favicon.png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+<style>
+body {
+    margin: 0;
+    font-family: Nunito,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #343a40;
+    text-align: left;
+    background-color: #fff;
+}
+</style>
+</head>
+<body class="bg-light">
+	<div class="container mt-3 mb-5">
+		<div class="row">
+			<div class="col-md-8 offset-md-2">
+				<h2 class="text-center" style="font-weight: 800;">CUCI GUDANG GILA-GILAAN</h2>
+				<h4 class="text-center">Dapatkan Produk Terbaik dengan Harga GILA-GILAAN, Silahkan Pilih dan kontak CS Kami untuk membeli produk</h4>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3 mt-3">
+				<div class="card">
+					<img src="https://m.dropy.id/wp-content/uploads/2019/12/skmei-1231-2.png" class="card-img-top" height="250">
+				  <div class="card-body">
+				    <h5 class="card-title">Jam Tangan Brush V2</h5>
+				    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing eli.</p>
+				    <div class="text-center">
+				    	<a href="#" class="btn btn-success">WhatsApp Sekarang</a>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
